4aa5b62ac3ab930c6115ec52964d67d71a1d55b6
cutlake2000 <cutlake2000@naver.com>
2023-10-14 22:20:35 +0900

    [DEBUG] 레그돌 회전 에러

---
+++ Assets/Scripts/Entities/Ragdoll/Grippable.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ActiveRagdoll {
+    // Author: Sergio Abreu García | https://sergioabreu.me
+
+    public class Grippable : MonoBehaviour {
+        public JointMotionsConfig jointMotionsConfig;
+    }
+} // namespace ActiveRagdolla
