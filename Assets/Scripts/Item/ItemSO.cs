3f3bc01b2038e150337e59a2f321087172fcce69
jyo217 <solbaram217@gmail.com>
2023-10-15 18:04:00 +0900

    [UPDATE] ItemSO 기본 틀 완성 #2

--- Assets/Scripts/Item/ItemSO.cs
+++ Assets/Scripts/Item/ItemSO.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemSO : MonoBehaviour
+public enum ItemType
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    Box,
+    Plate,
+    Tool,
+    ScoreObject,
 }
+
+[CreateAssetMenu(fileName = "ItemSO", menuName = "Item/ItemSO")]
+public class ItemSO : ScriptableObject
+{
+    [Header("Info")]
+    [SerializeField] private ItemType itemType;
+    [SerializeField] private float weight;
+}
+
+
